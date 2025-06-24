94352c559b790c3251dc9ca8201d75a65ad0366d
Oliver Azevedo Barnes <oliver@liquidvoting.io>
2025-06-23 22:40:10 -0300

    Use sqlite store
    Struggling to fix dependencies when importing the local pavex_session_sqlx crate with the changes:
    
    ```
    # Cargo.toml
    pavex_session = { path = "../../libs/pavex_session" }
    pavex_session_sqlx = { path = "../../libs/pavex_session_sqlx", features = [
        "sqlite",
    ] }
    # I'm importing pavex and others to appease the compiler complaining about
    # pavex dependencies in pavex_session_sqlx coming from two different versions of pavex
    pavex = { path = "../../libs/pavex" }
    pavex_tracing = { path = "../../libs/pavex_tracing" }
    pavex_cli_client = { path = "../../libs/pavex_cli_client" }
    ```
    
    Build error:
    
    ```
    ➜  demo git:(main) ✗ cargo px run
       Computing package graph
        Computed package graph in 0.417s
       Compiling `bp`, the code generator for `server_sdk`
       Compiling app v0.1.0 (/Users/oliver/Repos/pavex/demo/app)
    error[E0432]: unresolved import `pavex::kit`
     --> app/src/blueprint.rs:2:12
      |
    2 | use pavex::kit::ApiKit;
      |            ^^^ could not find `kit` in `pavex`
    ```

--- app/src/blueprint.rs
+++ app/src/blueprint.rs
@@ -1,20 +1,19 @@
 use crate::{configuration, ops, routes, telemetry};
 use pavex::kit::ApiKit;
 use pavex::{blueprint::Blueprint, cookie::CookieKit};
-use pavex_session_sqlx::PostgresSessionKit;
+use pavex_session_sqlx::SqliteSessionKit;
 
 /// The main blueprint, containing all the routes, middlewares, constructors and error handlers
 /// required by our API.
 pub fn blueprint() -> Blueprint {
     let mut bp = Blueprint::new();
-    ApiKit::new().register(&mut bp);
-    PostgresSessionKit::new().register(&mut bp);
+    SqliteSessionKit::new().register(&mut bp);
     // Sessions are built on top of cookies,
     // so you need to set those up too.
     // Order is important here!
     CookieKit::new().register(&mut bp);
 
-    bp.prebuilt(pavex::t!(sqlx::pool::Pool<sqlx::Postgres>));
+    bp.prebuilt(pavex::t!(sqlx::pool::Pool<sqlx::Sqlite>));
     // bp.prefix("/ops").nest(crate::ops::blueprint());
 
     telemetry::register(&mut bp);
--- app/src/configuration.rs
+++ app/src/configuration.rs
@@ -2,18 +2,18 @@ use pavex::blueprint::Blueprint;
 use pavex::server::IncomingStream;
 use pavex::t;
 use pavex::time::SignedDuration;
-use secrecy::{ExposeSecret, SecretString};
+use pavex_session_sqlx::SqliteSessionStore;
 use serde::Deserialize;
 use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::ConnectOptions;
-use sqlx::postgres::{PgConnectOptions, PgPool, PgPoolOptions, PgSslMode};
+use sqlx::sqlite::{SqliteConnectOptions, SqlitePool, SqlitePoolOptions};
 
 /// Refer to Pavex's [configuration guide](https://pavex.dev/docs/guide/configuration) for more details
 /// on how to manage configuration values.
 pub fn register(bp: &mut Blueprint) {
     bp.config("server", t!(self::ServerConfig));
     bp.config("database", t!(self::DatabaseConfig));
-    // bp.singleton(f!(crate::configuration::DatabaseConfig::get_pool));
+    bp.singleton(pavex::f!(self::DatabaseConfig::get_pool));
 }
 
 #[derive(serde::Deserialize, Debug, Clone)]
@@ -66,42 +66,30 @@ impl ServerConfig {
 // struct type to represent the database configuration
 #[derive(Clone, Debug, Deserialize)]
 pub struct DatabaseConfig {
-    pub username: String,
-    pub password: SecretString,
-    #[serde(deserialize_with = "deserialize_number_from_string")]
-    pub port: u16,
-    pub host: String,
-    pub database_name: String,
-    pub require_ssl: bool,
+    pub database_path: String,
 }
 
 // methods for the database configuration type
 impl DatabaseConfig {
-    pub fn with_db(&self) -> PgConnectOptions {
-        let options = self.without_db().database(&self.database_name);
-        options
-            .clone()
-            .log_statements(tracing_log::log::LevelFilter::Trace);
-
-        options
+    pub fn connect_options(&self) -> SqliteConnectOptions {
+        SqliteConnectOptions::new()
+            .filename(&self.database_path)
+            .create_if_missing(true)
+            .log_statements(tracing_log::log::LevelFilter::Trace)
     }
 
-    pub fn without_db(&self) -> PgConnectOptions {
-        let ssl_mode = if self.require_ssl {
-            PgSslMode::Require
-        } else {
-            PgSslMode::Prefer
-        };
-        PgConnectOptions::new()
-            .host(&self.host)
-            .username(&self.username)
-            .password(self.password.expose_secret())
-            .ssl_mode(ssl_mode)
-    }
-
-    pub async fn get_pool(&self) -> PgPool {
-        PgPoolOptions::new()
+    pub async fn get_pool(&self) -> SqlitePool {
+        let pool = SqlitePoolOptions::new()
             .acquire_timeout(std::time::Duration::from_secs(2))
-            .connect_lazy_with(self.with_db())
+            .connect_lazy_with(self.connect_options());
+
+        // Run migrations
+        let store = SqliteSessionStore::new(pool.clone());
+        store
+            .migrate()
+            .await
+            .expect("Failed to run SQLite migrations");
+
+        pool
     }
 }
